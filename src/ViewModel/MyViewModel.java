8738d9defee9383ba7af9dced754f5f0c9d1ad45
idanr <idan.razy@gmail.com>
2017-06-16 19:44:11 +0300

    add save + open options

--- src/ViewModel/MyViewModel.java
+++ src/ViewModel/MyViewModel.java
@@ -2,14 +2,13 @@ package ViewModel;
 
 
 import View.MazeDisplayer;
-import algorithms.mazeGenerators.AMazeGenerator;
 import algorithms.mazeGenerators.Maze;
-import algorithms.mazeGenerators.SimpleMazeGenerator;
+import algorithms.mazeGenerators.MyMazeGenerator;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.FileChooser;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,26 +16,59 @@ public class MyViewModel implements Initializable {
 
      @FXML
      MazeDisplayer mazeDisplay;
-     SimpleMazeGenerator maze=new SimpleMazeGenerator();
+    MyMazeGenerator maze=new MyMazeGenerator();
+    Maze m;
 
     //not happen just when initilaize, also we can call to initilize when we need
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Maze m= maze.generate(5,5);
-        mazeDisplay.setMazeData(m.getMaze());
+
 
 
     }
 
     public void start() {
-        System.out.println("Hey!");
-    }
 
-    public void newGame() {
-
-
-
-
+        Maze m= maze.generate(15,15);
+        mazeDisplay.setMazeData(m.getMaze());
+        
 
     }
+
+    public void openFIle() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("open Maze File");
+        fc.setInitialDirectory(new File("C:\\Users\\idanr\\Desktop\\src\\resources"));
+        File chosen = fc.showOpenDialog(null);
+        if(chosen!=null)
+            try {
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(chosen.getAbsoluteFile()));
+                byte[] mazearr =(byte[])in.readObject();
+                m = new Maze(mazearr);
+                mazeDisplay.setMazeData(m.getMaze());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+    }
+
+    public void saveFile(){
+
+         FileChooser fs = new FileChooser();
+         fs.setTitle("save Maze file");
+         fs.setInitialDirectory(new File("C:\\Users\\idanr\\Desktop\\src\\resources"));
+         File chosen = fs.showSaveDialog(null);
+         try {
+             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(chosen.getAbsoluteFile()));
+             out.writeObject(m);
+         }
+         catch (IOException e){
+             e.printStackTrace();
+         }
+
+
+    }
+
 }
